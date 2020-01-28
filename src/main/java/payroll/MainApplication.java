1ca1c0d13e40ac37c860f86e93148eac0b1fef97
yyoel <yoelyulius@gmail.com>
2020-01-28 07:14:22 +0000

    change topic listen + progress on storing data

--- src/main/java/payroll/MainApplication.java
+++ src/main/java/payroll/MainApplication.java
@@ -1,73 +1,53 @@
 package payroll;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.context.annotation.Bean;
-
-import javax.persistence.Persistence;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-//import java.util.logging.Logger;
-import java.sql.Connection;
-import java.sql.DriverManager;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class MainApplication {
 
     public static Logger logger = LoggerFactory.getLogger(MainApplication.class);
-
-//    @Autowired
-//    TransactionRepository repository;
+    private TransactionRepository repository;
 
     public static void main(String... args) {
         SpringApplication.run(MainApplication.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner dmeo(TransactionRepository repository){
-//        return (args -> {
-//            String from_date = "1997-07-17";
-//            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-//            Date fd = formatter.parse(from_date);
-//            java.sql.Date sqlDate = new java.sql.Date(fd.getTime());
-//            repository.save(new TransactionModel(sqlDate,1,"Hello"));
-//
-//            logger.info("GetAllTransaction");
-//            logger.info("-----------------");
-//            for (TransactionModel transactionModel : repository.findAll()){
-//                logger.info(toString());
-//            }
-//            logger.info("");
-//        });
-//    }
-
-    @KafkaListener(topics = "engineOneTopic")
-    public void listen(ConsumerRecord<?, ?> cr) throws Exception{
+    @KafkaListener(topics = "publishDataTopic")
+    public void listen(String cr) throws Exception{
         logger.info(cr.toString());
+ 
+        TransactionModel transaction = new Gson().fromJson(cr, TransactionModel.class);
+        logger.info("StartSave");
+        saveRepo(transaction);
+        logger.info("EndSave");
     }
 
-    @Bean
-    public String saveRepo(TransactionRepository repository) throws ParseException {
+    public String saveRepo(TransactionModel transactionModel) throws ParseException {
         TransactionModel transaction = new TransactionModel();
 
-        String from_date = "1997-07-17";
+        String pattern = ("yyyy-MM-dd");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        String date = simpleDateFormat.format(transaction.transactionTime);
         DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        Date fd = formatter.parse(from_date);
+        Date fd = formatter.parse(date);
         java.sql.Date sqlDate = new java.sql.Date(fd.getTime());
+
         transaction.setTransactionTime(sqlDate);
-        transaction.setProductId(2);
-        transaction.setBodyMessage("Hello World");
+        transaction.setProductId(transactionModel.productId);
+        transaction.setBodyMessage(transactionModel.bodyMessage);
         repository.save(transaction);
         return  "Saved";
     }
